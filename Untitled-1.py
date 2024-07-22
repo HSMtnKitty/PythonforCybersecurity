64f5c97398b4403607f0109688ff9fc3e1d3c81a
HSMtnKitty <shilling.heather@student.greenriver.edu>
2024-07-22 08:55:46 -0700

    Added chap2 assignment

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+#this file takes a user name, saves and prints it
+name = input("What is your name?")
+
+#prints the name suggested above
+print(f"Hello, {name}!") 
+
+#Prints a positive affirmation
+print("You're gonna do great today!")
+
+
+
+
