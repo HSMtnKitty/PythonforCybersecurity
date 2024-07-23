f73dcb74812562b09dc9cb907b7d86eab2fb8c0a
HSMtnKitty <shilling.heather@student.greenriver.edu>
2024-07-22 17:20:51 -0700

    Conditionals

---
+++ start/CH02/#Lesson 2.py
@@ -0,0 +1,8 @@
+#Lesson 2
+
+# print a loop "heck yeah!" 10 times
+for _ in range(10):
+    print ("heck yeah!")
+
+#taek an input from user of grade
+grade = int(input("Enter your grade: "))
--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 # example workign with conditionals
 #By 
+
+#Lesson 2
+
+# print a loop "heck yeah!" 10 times
+for _ in range(10):
+    print ("heck yeah!")
+
+#taek an input from user of grade
+grade = int(input("Enter your grade: "))
