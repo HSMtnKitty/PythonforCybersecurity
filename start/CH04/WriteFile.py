3702324cf631f15a880d3496cca0e105f47d0aa5
HSMtnKitty <shilling.heather@student.greenriver.edu>
2024-08-23 17:08:00 -0700

    more updates

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -8,10 +8,21 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 #open file fkor writing
 f = open(dir_path + "/hackme.txt", "w")
 
-#write to the file
-print("hello world\n")
-f.write("")
+#ask some questions
 
+name = input("What is your name?")
+color = input("What is your favorite color?")
+pet = input("What was your first pet's name?")
+mum = input("What is your mother's maiden name?")
+school = input("What is your first school's name?")
+
+#write to the file
+print("thank you!\n")
+f.write(name)
+f.write(color)
+f.write(pet)
+f.write(mum)
+f.write(school)
 
 #close file
 f.close()
