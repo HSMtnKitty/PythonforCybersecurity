1d03241202f6e613d8b831715ea237bc1ee6c8c7
HSMtnKitty <shilling.heather@student.greenriver.edu>
2025-02-18 18:26:37 -0800

    updated code

--- Assignments 1-5/Conditional.py
+++ Assignments 1-5/Conditional.py
@@ -1,5 +1,7 @@
 response = input("Is today a good day? y/n")
 if response == 'y':
     print("Yes it is")
-else:
+elif response == 'n':
     print("it will get better")
+else:
+    print("Please reinput y or n")
