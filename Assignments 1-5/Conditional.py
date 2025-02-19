21c332ab023697b703db3671bd784ab707a7672a
HSMtnKitty <shilling.heather@student.greenriver.edu>
2025-02-18 18:04:45 -0800

    Week 3 practice updates

---
+++ Assignments 1-5/Conditional.py
@@ -0,0 +1,5 @@
+response = input("Is today a good day? y/n")
+if response == 'y':
+    print("Yes it is")
+else:
+    print("it will get better")
---
+++ Assignments 1-5/Helloworld.py
@@ -0,0 +1,6 @@
+name = input("Insert your name here:")
+
+print("Hello", name)
+
+print("You are going to do amazing <3")
+
